48cae9cf4b698b8339590eb96e08cd1ffec38988
michael-maderich <52681233+michael-maderich@users.noreply.github.com>
2021-02-23 18:59:27 -0500

    Add files via upload

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -4,8 +4,15 @@ function playGame() {
     do {
         numRounds = prompt("How many rounds would you like to play?");
         console.log(numRounds);
-    } while ((numRounds === undefined) || (numRounds === null) || (parseInt(numRounds) === NaN));
-    do {
-        var player = prompt("Choose one of: Rock, Paper, Scissors");
-    } while (false);
+        numRounds = parseInt(numRounds);
+        console.log(numRounds);
+    } while ((numRounds === undefined) || (numRounds === null) || (numRounds === NaN));
+    console.log("Out of the loop");
+    for (var rnd=0; rnd < numRounds; rnd++) {
+        var playerChoice;
+        do {
+            playerChoice = prompt(`Round ${rnd+1}! Choose one of: Rock, Paper, Scissors`);
+            console.log("CHoice: "+playerChoice);
+        } while ((playerChoice === undefined) || (playerChoice === null) || (parseInt(playerChoice) === NaN));
+    }
 }
